8c8a9d59d036a4c2f9bb60e508e6db8117aa31fc
SeerLite <seerlite@disroot.org>
2024-09-21 23:16:02 -0300

    refactor(scraping): Make scrape action a function

--- scraping/scrape.py
+++ scraping/scrape.py
@@ -23,64 +23,47 @@ headers = {
 }
 
 endpoint = "https://api.axon.es/graphql"
-with open(Path("scraping") / "query.graphql") as query_file:
-    query = query_file.read()
 
-variables = {
-    "limit": 10,
-    "skip": 0,
-    # "sort": "book.edition.year",
-    # "order": -1,
-    # "product_type": ["book", "ebook"],
-    # "lang": None,
-    # "rangePrice": None,
-    # "areas_of_interest": None,
-    # "tags": None,
-    # "type": "all",
-    # "area_prox": None
-}
-payload = {
-    "operationName": "currentProducts",
-    "variables": variables,
-    "query": query,
-}
-# payload = json.loads('{"operationName":"currentProducts","variables":{"limit":10,"skip":0,"sort":"book.edition.year","order":-1,"product_type":["book"],"lang":null,"rangePrice":null,"areas_of_interest":null,"tags":null,"type":"all","area_prox":null},"query":"query currentProducts($limit: Int, $skip: Int!, $sort: String, $order: Int, $product_type: [String], $lang: [String], $rangePrice: RangePrice, $areas_of_interest: [Int], $areas_of_interest_text: String, $tags: [TagsInput], $type: String, $searchGeneral: String, $prox: Boolean, $area_prox: String, $featured: Boolean, $is_new: Boolean) {\\n  books(\\n    limit: $limit\\n    skip: $skip\\n    sort: $sort\\n    order: $order\\n    product_type: $product_type\\n    lang: $lang\\n    rangePrice: $rangePrice\\n    areas_of_interest: $areas_of_interest\\n    areas_of_interest_text: $areas_of_interest_text\\n    tags: $tags\\n    type: $type\\n    searchGeneral: $searchGeneral\\n    prox: $prox\\n    area_prox: $area_prox\\n    featured: $featured\\n    is_new: $is_new\\n  ) {\\n    id\\n    _id\\n    titleFriendly\\n    title\\n    ean\\n    mainImg\\n    ebookContent\\n    relatedInfo {\\n      _id\\n      id\\n      product_type\\n      priceWithDiscount\\n      ean\\n      title\\n      titleFriendly\\n      ebookContent\\n      weight\\n      prices {\\n        sale\\n        clearing\\n        saleSpecialDiscount\\n        ssdFrom\\n        ssdTo\\n        __typename\\n      }\\n      book {\\n        authors {\\n          _id\\n          name\\n          __typename\\n        }\\n        pages\\n        lang\\n        binding\\n        __typename\\n      }\\n      availability {\\n        OK\\n        msg\\n        status\\n        __typename\\n      }\\n      __typename\\n    }\\n    prices {\\n      sale\\n      clearing\\n      saleSpecialDiscount\\n      ssdFrom\\n      ssdTo\\n      __typename\\n    }\\n    vat {\\n      sale\\n      __typename\\n    }\\n    tags {\\n      category\\n      items\\n      __typename\\n    }\\n    book {\\n      authors {\\n        _id\\n        name\\n        __typename\\n      }\\n      lang\\n      binding\\n      edition {\\n        year\\n        number\\n        month\\n        __typename\\n      }\\n      __typename\\n    }\\n    product_type\\n    priority\\n    priority_nov\\n    priceWithDiscount\\n    not_released\\n    __typename\\n  }\\n}\\n"}')
-query_limit = 1000
-# query_limit = 100
-initial_skip = 0
-variables["limit"] = query_limit
-books = []
-i = 0
-accumulated_skip = 0
-try:
-    while True:
-        print(f"Iteration {i}")
-        print(f"{query_limit = }")
-        variables["skip"] = initial_skip + accumulated_skip
-        request = Request(endpoint, data=json.dumps(payload, separators=(",", ":")).encode(), headers=headers)
-        with urlopen(request) as response:
-            result = json.loads(response.read())
-        if not result["data"]["books"]:
-            break
-            # if query_limit == 1:
-            #     break
-            # else:
-            #     query_limit //= 2
-        else:
-            # books += [book for book in result["data"]["books"] if book["stock_available"] and book["product_type"] in ["book", "ebook"]]
-            books += [book for book in result["data"]["books"] if book["stock_available"] and book["product_type"] == "book"]
-            print(f"Saving {len(books)} so far")
-            accumulated_skip += query_limit
-            # tmp:
-            with open("libros.json", "w") as file:
-                json.dump(books, file)
-        i += 1
+def scrape(query_limit=1000):
+    with open(Path("scraping") / "query.graphql") as query_file:
+        query = query_file.read()
+    variables = {
+        "limit": query_limit,
+        "skip": 0,
+    }
+    payload = {
+        "operationName": "currentProducts",
+        "variables": variables,
+        "query": query,
+    }
+    initial_skip = 0
+    books = []
+    i = 0
+    accumulated_skip = 0
+    try:
+        while True:
+            print(f"Iteration {i}")
+            print(f"{query_limit = }")
+            variables["skip"] = initial_skip + accumulated_skip
+            request = Request(endpoint, data=json.dumps(payload, separators=(",", ":")).encode(), headers=headers)
+            with urlopen(request) as response:
+                result = json.loads(response.read())
+            if not result["data"]["books"]:
+                break
+            else:
+                books += [book for book in result["data"]["books"] if book["stock_available"] and book["product_type"] == "book"]
+                print(f"Saving {len(books)} so far")
+                accumulated_skip += query_limit
+            i += 1
 
-    with open("libros.json", "w") as file:
-        json.dump(books, file)
-except HTTPError as http_error:
-    if http_error.code != 400:
-        raise http_error
-    errors = json.loads(http_error.read())["errors"]
-    for error in errors:
-        print(error["message"])
+        with open("libros.json", "w") as file:
+            json.dump(books, file)
+    except HTTPError as http_error:
+        if http_error.code != 400:
+            raise http_error
+        errors = json.loads(http_error.read())["errors"]
+        for error in errors:
+            print(error["message"])
+
+if __name__ == "__main__":
+    scrape()
